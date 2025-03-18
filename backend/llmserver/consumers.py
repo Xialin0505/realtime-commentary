26b5d311f2acf3b9371aaeb212659f8b624b1abf
Xialin0505 <karen.liu0005@gmail.com>
2025-03-18 00:16:58 -0400

    not hardcode ip

--- backend/llmserver/consumers.py
+++ backend/llmserver/consumers.py
@@ -69,7 +69,10 @@ def process_img_compress(img_path):
 
 async def async_deepseek_generator(image_path):
     # Replace with your VM's external IP
-    url = "http://34.16.192.38:8000/inference_file"
+    from dotenv import load_dotenv
+    load_dotenv()
+    
+    url = "http://{}:8000/inference_file".format(api_key=os.environ.get("DEEPSEEK_IP"))
 
     # Construct the conversation payload as a JSON string.
     # The conversation should have an image placeholder for the image you are sending.
